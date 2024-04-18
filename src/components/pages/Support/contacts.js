da46a93878c346f167e34a597e0991abe50c1197
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-04-18 20:57:55 +0300

    support page

---
+++ src/components/pages/Support/contacts.js
@@ -0,0 +1,31 @@
+import gmail from '../../../assets/gmail.png';
+import telegram from '../../../assets/telegram.png';
+import instagram from '../../../assets/instagram.png';
+import telephone from '../../../assets/telephone.png';
+
+export const contacts = [
+    {
+        service: 'Gmail',
+        icon: gmail,
+        link: 'mailto:vadimvlad20032002@gmail.com',
+        text: 'vadimvlad20032002@gmail.com'
+    },
+    {
+        service: 'Telegram',
+        icon: telegram,
+        link: 'https://t.me/Stepan_Debela_official',
+        text: 'Vadym ➔ Harmash'
+    },
+    {
+        service: 'Instagram',
+        icon: instagram,
+        link: 'https://www.instagram.com/vadym_harmash_/',
+        text: 'vadym_harmash_'
+    },
+    {
+        service: 'Telephone',
+        icon: telephone,
+        link: 'tel:+380955981258',
+        text: '+380955981258'
+    }
+]
--- src/components/pages/Support/index.jsx
+++ src/components/pages/Support/index.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles/index.module.scss';
+import { contacts } from './contacts';
 
 export default function Support() {
     return (
-        <div>Support</div>
+        <div className={styles.support}>
+            <div className="container">
+                <div className={styles.support__wrapper}>
+                    <h1>За питаннями ви можете звернутись до нас в:</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                {contacts.map(contact => (
+                                    <th key={contact.service}>
+                                        <div>
+                                            <img src={contact.icon} alt={contact.service} />
+                                            <span>{contact.service}</span>
+                                        </div>
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                {contacts.map(contact => (
+                                    <td key={contact.service}>
+                                        <a href={contact.link} target='_blank'>{contact.text}</a>
+                                    </td>
+                                ))}
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     )
 }
