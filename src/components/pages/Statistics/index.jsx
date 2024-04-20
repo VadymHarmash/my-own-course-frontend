5a704a77d7c2fd833faf7e366dde6da4df6cf604
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-04-20 11:40:29 +0300

    about us page

--- src/components/pages/Statistics/index.jsx
+++ src/components/pages/Statistics/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import styles from './styles/index.module.scss'
 
 export default function Statistics() {
     return (
-        <div>Statistics</div>
+        <div className={styles.statistics}>
+            <div className="container">
+                <div className={styles.statistics__wrapper}>
+                    Stats
+                </div>
+            </div>
+        </div>
     )
 }
